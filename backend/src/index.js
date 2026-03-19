ae2b36b60c88327c5dcee670abd59c7a22005ffc
szywat <szywat04@gmail.com>
2026-03-19 19:00:30 +0100

    Basic Express.js template

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/items', (req, res) => {
+
+});
+
+app.post('/items', (req, res) => {
+
+});
+
+app.get('/stats', (req, res) => {
+
+});
+
+app.listen(port);

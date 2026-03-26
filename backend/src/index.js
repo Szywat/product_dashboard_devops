9f6f4c8f4b55ebed7ba2fb022887bda42a3cccd3
szywat <szywat04@gmail.com>
2026-03-26 14:14:24 +0100

    Created temporary backend with in-memory database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
-const port = 3000;
+const port = 8000;
+
+app.use(express.json())
+
+// Temporary db
+let items = [];
+
+const instanceId = crypto.randomUUID();
 
 app.get('/items', (req, res) => {
-
+    res.json(items)
 });
 
 app.post('/items', (req, res) => {
+    const newItem = req.body;
 
+    if (!newItem || Object.keys(newItem).length === 0) {
+        return res.status(400).json({error: 'Brak danych produktu'});
+    }
+
+    items.push(newItem)
+    res.status(201).json(newItem)
 });
 
 app.get('/stats', (req, res) => {
-
+    res.json({
+        totalItems: items.length,
+        instanceId: instanceId
+    });
 });
 
 app.listen(port);
